568cf3bb459316ad103d80558ed222fb77993faa
sahilsaini29 <41050996+sahilsaini29@users.noreply.github.com>
2020-12-17 13:36:38 +0530

    Add files via upload

---
+++ Python Assignment 1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+for i in range(2000,3200):
+    if i%7==0 and i%5!=0:
+        print(i,end= ',')
+
+
+# In[18]:
+
+
+a = input("enter your name ")
+print(a[::-1])
+
+
+# In[19]:
+
+
+pi=3.14
+r=int(input("enter the radius "))
+v=(4/3)*(pi)*(r*r*r)
+print("The volume of sphere is: ",v)
+
+
+# In[ ]:
+
+
+
+
